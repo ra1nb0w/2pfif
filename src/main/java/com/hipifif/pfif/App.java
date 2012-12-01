2e0665f59725171098709d023b988a2b572bf53a
rainbow <rainbow@irh.it>
2012-12-01 15:30:46 +0100

    maven added

---
+++ src/main/java/com/hipifif/pfif/App.java
@@ -0,0 +1,13 @@
+package com.hipifif.pfif;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
